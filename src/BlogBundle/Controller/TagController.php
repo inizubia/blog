4fda1220d49f9172ea5462f6245243ed3122141f
inizubia <inigozubia@gmail.com>
2017-10-11 17:57:21 +0200

    71.ena

--- src/BlogBundle/Controller/TagController.php
+++ src/BlogBundle/Controller/TagController.php
@@ -4,45 +4,37 @@ namespace BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use BlogBundle\Entity\User;
-use BlogBundle\Form\UserType;
+use Symfony\Component\HttpFoundation\Session\Session;
+use BlogBundle\Entity\Tag;
+use BlogBundle\Form\TagType;
 
-class UserController extends Controller
+class TagController extends Controller
 {
-    public function loginAction(Request $request){
-        $authenticationUtils = $this->get("security.authentication_utils");
-        $error = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastUsername();
-        
-        $user = new User();
-        $form = $this->createForm(UserType::class,$user);
+    private $session;
+    
+    public function __construct() {
+        $this->session=new Session();
+    }
+
+    public function addAction(Request $request){
+        $tag = new Tag();
+        $form = $this->createForm(TagType::class, $tag);
         
         $form->handleRequest($request);
         
-        if($form->isValid()){
-             $user = new User();
-             $user->setName($form->get("name")->getData());
-             $user->setSurname($form->get("surname")->getData());
-             $user->setEmail($form->get("email")->getData());
-             $user->setPassword($form->get("password")->getData());
-             $user->setRole("ROLE_USER");
-             $user->setImagen(null);
-             
-             $em = $this->getDoctrine()->getManager();
-             $em->persist($user);
-             $flush = $em->flush();
-             if($flush == null)
-                $status = "El usuario se ha creado correctamente.";
-             else
-                $status = "No te has registrado correctamente";
-        }else{
-            $status = "No te has registrado correctamente";
+        if($form->isSubmitted()){
+            if($form->isValid()){
+                $status = "La etiqueta se ha creado correctamente!!";
+            }
+            else{
+                $status = "La etiqueta no se ha creado, porque el formulario no es valido";
+            }
         }
         
-        return $this->render("BlogBundle:User:login.html.twig", array(
-            "error" => $error,
-            "last_username" => $lastUsername,
-            "form" => $form->createView()
+        $this->session->getFlashBag()->add("status",$status);
+        
+        return $this->render("BlogBundle:Tag:add.html.twig",array(
+            "form"=>$form->createView()
         ));
     }
 }
